75567fd8bbaf80a22c06b3155d3c6546a9f1b22e
DaikiKaminari <daikikaminari@gmail.com>
2021-07-19 01:45:28 +0200

    Fix

--- visitors.lua
+++ visitors.lua
@@ -6,13 +6,15 @@ local function getSkillsAndCost(colony)
     local visitorsSkills = {}
     local skills
     local visitors = colony.getVisitors()
-    for visitor in pair(visitors) do
+    for _, visitor in pair(visitors) do
         infos = "Cost : " .. visitor["cost"]["displayName"] .. " (" .. tostring(visitor["cost"]["count"]) ..")\n"
         for skill_name, values in pairs(visitor["skills"]) do
-            infos = infos + skill_name + " : " .. string.rep(" ", 10-string.len(skill_name)) .. tostring(values["level"]) .. "\n"
+            infos = infos .. skill_name .. " : " .. string.rep(" ", 10-string.len(skill_name)) .. tostring(values["level"]) .. "\n"
         end
         visitorsSkills[visitor["name"]] = infos
     end
     return visitorsSkills
 end
 v.getSkillsAndCost = getSkillsAndCost
+
+return v
